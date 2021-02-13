e92055e8655acf18bc4fb50a19a39fb34eba9e2f
darioala <darioala@localhost.localdomain>
2021-02-12 20:41:24 -0600

    add payments page and alert dialog

--- lib/payment.dart
+++ lib/payment.dart
@@ -11,6 +11,48 @@ class Payment extends StatefulWidget {
 }
 
 class _PaymentState extends State<Payment> {
+
+  void _showEventDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: ListTile(
+            title: Text('Orden Exitosa!',
+              style: TextStyle(fontSize: 24),
+            ),
+            leading : Icon(Icons.emoji_food_beverage,
+              size: 35,
+            ),
+            subtitle: Text("Tazas Cupping"),
+          ),
+          content: Text('Tu orden ha sido registrada, para mas informacion busca en la opcion historial de compras en tu perfil'),
+          actions: [
+            FlatButton(
+              textColor: Color(0xFF6200EE),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('Aceptar'),
+            ),
+          ],
+        );
+      }
+    );
+  }
+
+  var paymentMethods = [
+    
+      {'str' : "Tarjeta de credito" ,
+        'icon' : Icons.credit_card
+      },
+      {'str' : "PayPal",
+        'icon' : Icons.account_balance_wallet_rounded },
+      {'str' : "Tarjeta de regalo" ,
+        'icon' : Icons.card_giftcard},
+  ];
+  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -46,14 +88,18 @@ class _PaymentState extends State<Payment> {
           children: <Widget>[
             Padding(
               padding: const EdgeInsets.only(left: 30, bottom : 8),
-              child: Text("Elige tu metodo de pago"),
+              child: Text("Elige tu metodo de pago: ",
+                style : TextStyle(
+                  fontSize: 30.0,
+                ),
+              ),
             ),
             Container(
               height: MediaQuery.of(context).size.height * 0.7,
               child: ListView.builder(
                 itemCount: 3,
                 itemBuilder: (BuildContext context, int index) {
-                  return buildCardPayment();
+                  return buildCardPayment(paymentMethods[index]['icon'], paymentMethods[index]['str']);
                 },
               ),
             ),
@@ -63,25 +109,46 @@ class _PaymentState extends State<Payment> {
     );
   }
 
-  Card buildCardPayment() {
-    return Card(
-      color: Color(CUPPIN_GREY),
-      margin: EdgeInsets.only(top: 8, bottom: 8, left : 35, right : 35),
-      child: Row(
-        mainAxisSize: MainAxisSize.max,
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(left: 5),
-            child: Padding(
-              padding: const EdgeInsets.all(20),
-              child: Icon(
-                Icons.credit_card,
-                size: 75.0,
+  GestureDetector buildCardPayment(i,s) {
+    return GestureDetector(
+          child: Card(
+        color: Color(CUPPIN_GREY),
+        margin: EdgeInsets.only(top: 8, bottom: 8, left : 35, right : 35),
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 5),
+              child: Padding(
+                padding: const EdgeInsets.all(20),
+                child: Icon(
+                  i,
+                  size: 75.0,
+                ),
+              )
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top : 8, bottom : 8, left : 30),
+              child: Container(
+                width: 100,
+                child: Text(s,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                  ),
+                ),
               ),
-            )
-          )
-        ],
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top : 75, left: 30),
+              child: Icon(Icons.edit),
+            ),
+          ],
+        ),
       ),
+      onTap: () {
+        _showEventDialog(context);
+      },
     );
   }
 }
--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -18,6 +18,6 @@ const String PROFILE_PICTURE =
 const PRIMARY_COLOR = Colors.lightBlue;
 const SECONDARY_COLOR = Colors.grey;
 
-const CUPPIN_GREY = 0x8B8175;
+const CUPPIN_GREY = 0xFF8B8175;
 const CUPPIN_BLUE = 0xFF214254;
 const CUPPIN_ORANGE = 0xEC9762;

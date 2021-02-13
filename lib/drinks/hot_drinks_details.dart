6653091d235313dd54125cb2fb1a12eadc9be94c
chamedm <is708177@iteso.mx>
2021-02-12 18:37:42 -0600

    Adding HotDrinksDetails page with passed item thru route + using function to calculate final price

--- lib/drinks/hot_drinks_details.dart
+++ lib/drinks/hot_drinks_details.dart
@@ -10,22 +10,180 @@ class HotDrinksDetails extends StatefulWidget {
 }
 
 class _HotDrinksDetailsState extends State<HotDrinksDetails> {
+  var _smallSelected = true;
+  var _mediumSelected = false;
+  var _largeSelected = false;
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: ListView(children: <Widget>[
-        Container(
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(10),
-            child: Image.network(
-              "${widget.drink.productImage}",
-              fit: BoxFit.cover,
-              height: 100,
-              width: 100,
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("${widget.drink.productTitle}"),
+        ),
+        body: Container(
+          child: ListView(children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(
+                  right: 45, left: 45, top: 20, bottom: 20),
+              child: Container(
+                color: Color.fromRGBO(235, 170, 59, .8),
+                height: 320,
+                width: 230,
+                child: Align(
+                  alignment: Alignment.center,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: Image.network(
+                      "${widget.drink.productImage}",
+                      fit: BoxFit.cover,
+                      height: 200,
+                      width: 200,
+                    ),
+                  ),
+                ),
+              ),
             ),
-          ),
-        )
-      ]),
-    );
+            Padding(
+              padding: EdgeInsets.only(top: 20, left: 20),
+              child: Text(
+                "${widget.drink.productTitle}",
+                style: Theme.of(context)
+                    .textTheme
+                    .headline4
+                    .copyWith(fontWeight: FontWeight.bold),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 20, left: 20, right: 20),
+              child: Text(
+                "${widget.drink.productDescription}",
+                style: Theme.of(context)
+                    .textTheme
+                    .bodyText1
+                    .copyWith(fontSize: 18),
+              ),
+            ),
+            Row(children: [
+              Padding(
+                padding: EdgeInsets.only(top: 20, left: 20),
+                child: Text(
+                  "Tamaños disponibles",
+                  style: Theme.of(context)
+                      .textTheme
+                      .caption
+                      .copyWith(fontSize: 15),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 80, top: 20, right: 20),
+                child: Text(
+                  "\$\ ${widget.drink.productPrice}",
+                  style: Theme.of(context)
+                      .textTheme
+                      .bodyText2
+                      .copyWith(fontSize: 30),
+                ),
+              ),
+            ]),
+            Padding(
+              padding: EdgeInsets.only(left: 20, right: 80),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  MaterialButton(
+                      child: Text("Chico"),
+                      highlightElevation: 3,
+                      color: _smallSelected
+                          ? Color.fromRGBO(151, 181, 230, .8)
+                          : Colors.grey[300],
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                      onPressed: () => {
+                            setState(() {
+                              widget.drink.productSize = ProductSize.CH;
+                              widget.drink.productPrice =
+                                  widget.drink.productPriceCalculator();
+                              _smallSelected = true;
+                              _mediumSelected = false;
+                              _largeSelected = false;
+                            })
+                          }),
+                  MaterialButton(
+                      child: Text("Mediano"),
+                      highlightElevation: 3,
+                      color: _mediumSelected
+                          ? Color.fromRGBO(151, 181, 230, .8)
+                          : Colors.grey[300],
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                      onPressed: () => {
+                            setState(() {
+                              widget.drink.productSize = ProductSize.M;
+                              widget.drink.productPrice =
+                                  widget.drink.productPriceCalculator();
+                              _smallSelected = false;
+                              _mediumSelected = true;
+                              _largeSelected = false;
+                            })
+                          }),
+                  MaterialButton(
+                      child: Text("Grande"),
+                      highlightElevation: 3,
+                      color: _largeSelected
+                          ? Color.fromRGBO(151, 181, 230, .8)
+                          : Colors.grey[300],
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                      onPressed: () => {
+                            setState(() {
+                              widget.drink.productSize = ProductSize.G;
+                              widget.drink.productPrice =
+                                  widget.drink.productPriceCalculator();
+                              _smallSelected = false;
+                              _mediumSelected = false;
+                              _largeSelected = true;
+                            })
+                          })
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 20, right: 20, top: 40),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  MaterialButton(
+                      minWidth: 150,
+                      height: 50,
+                      color: Color.fromRGBO(188, 176, 161, 1),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                      child: Text(
+                        "Agregar al carrito",
+                        style: Theme.of(context)
+                            .textTheme
+                            .button
+                            .copyWith(color: Colors.white),
+                      ),
+                      onPressed: () => {}),
+                  MaterialButton(
+                      minWidth: 150,
+                      height: 50,
+                      color: Color.fromRGBO(188, 176, 161, 1),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                      child: Text(
+                        "Comprar ahora",
+                        style: Theme.of(context)
+                            .textTheme
+                            .button
+                            .copyWith(color: Colors.white),
+                      ),
+                      onPressed: () => {})
+                ],
+              ),
+            )
+          ]),
+        ));
   }
 }
--- lib/drinks/item_hot_drinks.dart
+++ lib/drinks/item_hot_drinks.dart
@@ -1,3 +1,4 @@
+import 'package:estructura_practica_1/drinks/hot_drinks_details.dart';
 import 'package:flutter/material.dart';
 import '../models/product_hot_drinks.dart';
 
@@ -17,96 +18,102 @@ class _ItemHotDrinksState extends State<ItemHotDrinks> {
   Widget build(BuildContext context) {
     // return Container(child: Text("${widget.drink.productTitle}"));
     return GestureDetector(
-      onTap: () => {print("tap")},
-      child: Container(
-        height: 220,
-        child: Stack(
-          children: [
-            Container(
-              height: 200,
-              margin: EdgeInsets.only(top: 28, bottom: 24, left: 24, right: 24),
-              decoration: BoxDecoration(
-                color: Color.fromRGBO(157, 147, 123, .8),
-                borderRadius: BorderRadius.all(
-                  Radius.circular(10),
+        child: Container(
+          height: 220,
+          child: Stack(
+            children: [
+              Container(
+                height: 200,
+                margin:
+                    EdgeInsets.only(top: 28, bottom: 24, left: 24, right: 24),
+                decoration: BoxDecoration(
+                  color: Color.fromRGBO(157, 147, 123, .8),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(10),
+                  ),
                 ),
+                child: ListView(
+                    physics: NeverScrollableScrollPhysics(),
+                    children: [
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            margin: EdgeInsets.only(left: 16, top: 16),
+                            width: 180,
+                            height: 70,
+                            decoration: BoxDecoration(
+                              color: Color.fromRGBO(230, 226, 218, 1),
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(10),
+                              ),
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.all(24.0),
+                              child: Text(
+                                "${widget.drink.productTitle}",
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .headline6
+                                    .copyWith(fontWeight: FontWeight.bold),
+                                textAlign: TextAlign.center,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            margin: EdgeInsets.only(left: 16, top: 16),
+                            width: 180,
+                            height: 50,
+                            decoration: BoxDecoration(
+                              color: Color.fromRGBO(230, 226, 218, 1),
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(10),
+                              ),
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.all(15.0),
+                              child: Text(
+                                " \$\ ${widget.drink.productPrice}",
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .subtitle1
+                                    .copyWith(fontWeight: FontWeight.bold),
+                                textAlign: TextAlign.center,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ]),
               ),
-              child:
-                  ListView(physics: NeverScrollableScrollPhysics(), children: [
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Container(
-                      margin: EdgeInsets.only(left: 16, top: 16),
-                      width: 180,
-                      height: 70,
-                      decoration: BoxDecoration(
-                        color: Color.fromRGBO(230, 226, 218, 1),
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(10),
-                        ),
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.all(24.0),
-                        child: Text(
-                          "${widget.drink.productTitle}",
-                          style: Theme.of(context)
-                              .textTheme
-                              .headline6
-                              .copyWith(fontWeight: FontWeight.bold),
-                          textAlign: TextAlign.center,
-                        ),
-                      ),
+              Align(
+                alignment: Alignment.topRight,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Padding(
+                    padding: EdgeInsets.only(top: 60, right: 40),
+                    child: Image.network(
+                      "${widget.drink.productImage}",
+                      fit: BoxFit.cover,
+                      height: 100,
+                      width: 100,
                     ),
-                  ],
-                ),
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Container(
-                      margin: EdgeInsets.only(left: 16, top: 16),
-                      width: 180,
-                      height: 50,
-                      decoration: BoxDecoration(
-                        color: Color.fromRGBO(230, 226, 218, 1),
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(10),
-                        ),
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.all(15.0),
-                        child: Text(
-                          " \$\ ${widget.drink.productPrice}",
-                          style: Theme.of(context)
-                              .textTheme
-                              .subtitle1
-                              .copyWith(fontWeight: FontWeight.bold),
-                          textAlign: TextAlign.center,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ]),
-            ),
-            Align(
-              alignment: Alignment.topRight,
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(10),
-                child: Padding(
-                  padding: EdgeInsets.only(top: 60, right: 40),
-                  child: Image.network(
-                    "${widget.drink.productImage}",
-                    fit: BoxFit.cover,
-                    height: 100,
-                    width: 100,
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
-      ),
-    );
+        onTap: () => {
+              Navigator.of(context).push(MaterialPageRoute(
+                  builder: (context) => HotDrinksDetails(
+                        drink: widget.drink,
+                      )))
+            });
   }
 }
